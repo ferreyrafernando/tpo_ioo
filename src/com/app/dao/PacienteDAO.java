dbfcde870980a2c476e1bf5a4c94ad3b64c1ba58
Flor Chao <85455479+Florencia260@users.noreply.github.com>
2021-10-28 11:58:27 -0300

    Reeplique el metodo "guardar" de UsuarioDAO en PacienteDAO

--- src/com/app/dao/PacienteDAO.java
+++ src/com/app/dao/PacienteDAO.java
@@ -1,12 +1,37 @@
 package com.app.dao;
 
 import com.app.negocio.Paciente;
+import com.app.negocio.Usuario;
+
 
 public class PacienteDAO implements IPacienteDAO{
 
     @Override
     public void guardar(Paciente paciente) {
+        ArchivoPaciente archivoP = new ArchivoPaciente("archivos/pacientes.txt");
+        ArrayList lista = archivoP.listar();
+        long max = 0;
 
+        if(paciente.getId() == 0) {
+            for(Object objeto: lista)
+            {
+                if(((Paciente) objeto).getId() > max)
+                    max = ((Paciente) objeto).getId();
+            }
+            paciente.setId(max + 1);
+            lista.add(paciente);
+        }
+        else{
+            for(Object objeto : lista){
+                Paciente us = (Paciente) objeto;
+                if(us.getId() == paciente.getId()){
+                    lista.remove(us);
+                    lista.add(paciente);
+                }
+            }
+        }
+
+        archivoP.guardar(lista);
     }
 
     @Override
