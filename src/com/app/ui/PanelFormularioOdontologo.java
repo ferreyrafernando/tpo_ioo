8d72f0eff132257eef997ba35880e4e15b0509ef
FacundoSantino <sanchezfacundosantino@gmail.com>
2021-11-17 11:39:41 -0300

    MOD PanelFormularioOdontologo

--- src/com/app/ui/PanelFormularioOdontologo.java
+++ src/com/app/ui/PanelFormularioOdontologo.java
@@ -2,8 +2,10 @@ package com.app.ui;
 
 import com.app.negocio.Odontologo;
 import com.app.service.OdontologoService;
+import sun.invoke.empty.Empty;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,10 +36,10 @@ public class PanelFormularioOdontologo extends JPanel {
         this.panelManager = panelManager;
         setLayout(new BorderLayout());
 
-        lblID = new JLabel("ID:",SwingConstants.CENTER);
-        lblNombre = new JLabel("Nombre:",SwingConstants.CENTER);
-        lblApellido = new JLabel("Apellido:",SwingConstants.CENTER);
-        lblMatricula = new JLabel("Matricula:",SwingConstants.CENTER);
+        lblID = new JLabel("ID:",SwingConstants.LEFT);
+        lblNombre = new JLabel("Nombre:",SwingConstants.LEFT);
+        lblApellido = new JLabel("Apellido:",SwingConstants.LEFT);
+        lblMatricula = new JLabel("Matricula:",SwingConstants.LEFT);
         txtID = new JTextField("", 3);
         txtNombre = new JTextField("", 10);
         txtApellido = new JTextField("", 10);
@@ -48,9 +50,8 @@ public class PanelFormularioOdontologo extends JPanel {
 
 
         JLabel titulo=new JLabel("Ingrese los datos",SwingConstants.CENTER);
-
+        titulo.setVerticalAlignment(SwingConstants.BOTTOM);
         titulo.setFont(new Font("Raleway",Font.BOLD,26));
-
 
         lblID.setFont(a);
         lblNombre.setFont(a);
@@ -62,17 +63,22 @@ public class PanelFormularioOdontologo extends JPanel {
 
         GridBagConstraints textos=new GridBagConstraints();
         GridBagConstraints formularios=new GridBagConstraints();
-        formularios.fill = GridBagConstraints.HORIZONTAL;
         formularios.ipady = 40;
         formularios.weightx = 2;
-        formularios.gridx=1;
+        formularios.gridx=2;
         formularios.gridwidth = 3;
         formularios.gridheight=2;
-        textos.fill = GridBagConstraints.HORIZONTAL;
+        formularios.insets=new Insets(0,0,0,50);
+        formularios.anchor= GridBagConstraints.WEST;
+        formularios.fill=GridBagConstraints.BOTH;
+
+        textos.anchor=GridBagConstraints.EAST;
+        textos.insets=new Insets(0,50,0,0);
         textos.weightx = 0.5;
-        textos.gridx=0;
+        textos.gridx=1;
         textos.gridheight=2;
         textos.gridwidth=1;
+        textos.fill=GridBagConstraints.BOTH;
 
         panelComponentes.add(lblID,textos);
         panelComponentes.add(txtID,formularios);
@@ -86,9 +92,29 @@ public class PanelFormularioOdontologo extends JPanel {
 
         btnGuardar = new JButton("Guardar");
         btnCancelar = new JButton("Cancelar");
+        btnCancelar.setFont(new Font("Raleway",Font.PLAIN,15));
+        btnGuardar.setFont(new Font("Raleway",Font.PLAIN,15));
+
         panelBotonera = new JPanel();
-        panelBotonera.add(btnGuardar);
-        panelBotonera.add(btnCancelar);
+        panelBotonera.setLayout(new GridBagLayout());
+
+        Dimension botones=new Dimension(150,45);
+
+        GridBagConstraints b1=new GridBagConstraints();
+        b1.gridx=1;
+        b1.insets=new Insets(0,0,0,20);
+        b1.fill=GridBagConstraints.BOTH;
+        btnGuardar.setPreferredSize(botones);
+
+        GridBagConstraints b2=new GridBagConstraints();
+        b2.gridx=2;
+        b2.fill=GridBagConstraints.BOTH;
+        btnCancelar.setPreferredSize(botones);
+
+
+
+        panelBotonera.add(btnGuardar,b1);
+        panelBotonera.add(btnCancelar,b2);
 
         add(panelComponentes, BorderLayout.CENTER);
         add(panelBotonera, BorderLayout.SOUTH);
